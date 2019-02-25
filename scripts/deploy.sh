fc3abd4a8c72ac902cd94085e9ff3986eac6dec2
Alex Melehy <amelehy@gmail.com>
2019-02-24 21:56:09 -0500

    added deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export AWS_PROFILE=chalice
+
+echo Checking out master and pulling down latest tag
+git checkout master && git pull && git fetch --tags
+CURRENT_TAG=`git describe --abbrev=0 --tags`
+
+echo Enter the git version tag for this release in the following format: v1.0.0
+echo The current version tag is: $CURRENT_TAG
+echo
+read NEW_TAG
+
+echo Enter a release message for this release:
+echo
+read RELEASE_MESSAGE
+
+# Frontend
+echo Deploying frontend app
+echo
+echo Running npm install, npm test, npm run build, pushing to S3, creating git tag and pushing to github
+
+npm install && npm run build && aws s3 sync build/ s3://motivora-website && git tag $NEW_TAG -m "$RELEASE_MESSAGE" && git push origin $NEW_TAG
+
+# Backend
+echo
+echo Deploying backend app
+
+chalice deploy
