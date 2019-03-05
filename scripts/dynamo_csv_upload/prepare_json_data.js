df46e2d67b010b244a5f7920a70b1607f8723bea
Alex Melehy <amelehy@gmail.com>
2019-03-05 17:30:47 -0500

    data upload scripts

--- scripts/dynamo_csv_upload/prepare_json_data.js
+++ scripts/dynamo_csv_upload/prepare_json_data.js
@@ -22,14 +22,15 @@ const parseAttributes = object => {
   };
 }
 
-const currentHighestId = 11;
+// Set this value to be the current highest ID of the messages in the table.
+const currentHighestId = 0;
 const finalData = data.map( (obj, idx) => {
   const attributes = parseAttributes(obj);
   return {
     "PutRequest": {
       "Item": {
         "id": {
-          "N": `${currentHighestId + idx}`
+          "N": `${currentHighestId + idx + 1}`
         },
         "body": {
           "S": obj.body
@@ -38,7 +39,7 @@ const finalData = data.map( (obj, idx) => {
           "S": "EBNHC"
         },
         "seq": {
-          "N": `${currentHighestId + idx}`
+          "N": `${currentHighestId + idx + 1}`
         },
         "total_attr": {
           "N": `${attributes.size}`
@@ -63,11 +64,17 @@ const finalData = data.map( (obj, idx) => {
   }
 });
 
-fs.writeFile("./data.json", JSON.stringify({
-  "motivora-messages": finalData
-}), err => {
-  if(err) {
-    return console.log(err);
-  }
-  console.log("The file was saved!");
+// Break data into chunks of 25 put requests
+// AWS has an abitrary limit: https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html
+const dataChunks = _.chunk(finalData, 25);
+_.each(dataChunks, (chunk, idx) => {
+  fs.writeFile(`./data_to_upload/data-${idx}.json`, JSON.stringify({
+    "motivora-messages": chunk
+  }), err => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
 });
+
--- scripts/dynamo_csv_upload/upload_messages.sh
+++ scripts/dynamo_csv_upload/upload_messages.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Prepare data files
 node prepare_json_data.js
-aws dynamodb batch-write-item --request-items file://data.json --region us-east-1 --profile chalice
+
+# Iterate through all data files and upload each
+# Ended up having to manually upload each one
+for filename in ./data_to_upload/*.json; do
+  sleep 1
+  echo $filename
+  aws dynamodb batch-write-item --request-items "file:/${filename:1}" --region us-east-1 --profile chalice
+done
