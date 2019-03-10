da6284be9d5fc2f213eda5be016b3aab28cc8c95
Alex Melehy <amelehy@gmail.com>
2019-03-10 19:59:26 -0400

    pr fixes and cleanup

--- scripts/dynamo_csv_upload/upload_messages.sh
+++ scripts/dynamo_csv_upload/upload_messages.sh
@@ -4,9 +4,11 @@
 node prepare_json_data.js
 
 # Iterate through all data files and upload each
-# Ended up having to manually upload each one
-for filename in ./data_to_upload/*.json; do
-  sleep 1
-  echo $filename
-  aws dynamodb batch-write-item --request-items "file:/${filename:1}" --region us-east-1 --profile chalice
-done
+# This loop ended up not working because AWS errored on some of the uploads.
+# Ended up just manually running the command for reach json file. Feel free
+# to try and get this loop running in the future if need be.
+# for filename in ./data_to_upload/*.json; do
+#   sleep 1
+#   echo $filename
+#   aws dynamodb batch-write-item --request-items "file:/${filename:1}" --region us-east-1 --profile chalice
+# done
