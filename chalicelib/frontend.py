f47fad4f15146fb8b6f47d990cde9e185dbf4a1c
Alex Melehy <amelehy@gmail.com>
2019-05-07 00:03:37 -0400

    update test endpoint

--- chalicelib/frontend.py
+++ chalicelib/frontend.py
@@ -83,12 +83,17 @@ def list_users():
 def test():
   user = Users.get(17602147229)
   user_obj = UserActions(**user.to_dict())
+  print("Hour: " + str(12), "Phone: " + str(user_obj.phone))
   # Only send for the first 28 days
-  if user_obj.sent_messages_length() >= user_obj.total_days: return 'Program ended (28 days)'
-  is_successful = user_obj.send_next_sms()
-  if is_successful:
-      user_obj.send_sms('How helpful was this message? [Scale of 0-10, with 0=not helpful at all and 10=very helpful]')
-      user_obj.set_next_message()
+  if user_obj.sent_messages_length() >= user_obj.total_days:
+      print('Program ended (28 days) ' + str(user_obj.phone))
+  else:
+      print('Sending message to ' + str(user_obj.phone))
+      is_successful = user_obj.send_next_sms()
+      if is_successful:
+          user_obj.set_next_message()
+      else:
+          sentry_sdk.capture_exception('Message sending unsuccessful for ' + str(user_obj.phone))
   return False
 
 #
