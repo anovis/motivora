202a8b02422d570a719a806a412655f5c71041b4
Alex Melehy <amelehy@gmail.com>
2019-05-23 11:02:07 -0400

    bump read capacity for tables

--- chalicelib/models/__init__.py
+++ chalicelib/models/__init__.py
@@ -4,10 +4,10 @@ from chalicelib.models.invocations import Invocations
 
 
 if not Messages.exists():
-    Messages.create_table(read_capacity_units=5, write_capacity_units=5, wait=True)
+    Messages.create_table(read_capacity_units=10, write_capacity_units=5, wait=True)
 
 if not Users.exists():
-    Users.create_table(read_capacity_units=5, write_capacity_units=5, wait=True)
+    Users.create_table(read_capacity_units=10, write_capacity_units=5, wait=True)
 
 if not Invocations.exists():
     Invocations.create_table(read_capacity_units=5, write_capacity_units=5, wait=True)
