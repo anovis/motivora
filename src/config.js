54758c4eae6e08a6c3ca5b26f1bb3210ccaa4c42
Jessica Long <jessica.muremure@gmail.com>
2020-07-24 17:48:51 +0000

    [JL] Update API Gateway reference

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const endpoint = (process.env.NODE_ENV === "development") ?
 	"http://127.0.0.1:8000" 
 		:
-	"https://sj0fyj60d0.execute-api.us-east-1.amazonaws.com/api";
+	"https://xct5xt77se.execute-api.us-east-1.amazonaws.com/api";
 
 const Config = {
   api: endpoint
