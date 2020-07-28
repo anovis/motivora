3ba071b2c23fb65aeadda222c9f6062631fb8046
karimou <karimou.ba@gmail.com>
2020-07-28 11:57:21 +0000

    [KBa] A web user can send a direct message to a participant

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 
 Amplify.configure({
     Auth: {
